725d8527080e389d0b0bce1ea99ec001e9afdd3a
Matt Wylder <matt@wylder.us>
2022-02-21 11:11:10 -0600

    Initial Commit

---
+++ InterNationBuilder ios/InterNationBuilder_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterNationBuilder_iosApp.swift
+//  InterNationBuilder ios
+//
+//  Created by Matthew Wylder on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct InterNationBuilder_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
